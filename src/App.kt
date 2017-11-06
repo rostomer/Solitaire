f0f2a593fae1fe65e531cf40ca0153c5293e7784
rostomer <rostomerr@gmail.com>
2017-11-06 18:06:11 +0300

    Initial commit

---
+++ src/App.kt
@@ -0,0 +1,6 @@
+/**
+ * Created by Asus on 02.11.2017.
+ */
+fun main(args: Array<String>) {
+
+}
---
+++ src/Card.kt
@@ -0,0 +1,4 @@
+/**
+ * Created by Asus on 02.11.2017.
+ */
+class Card(val value: Int, val suit: String, var faceup: Boolean = false)
